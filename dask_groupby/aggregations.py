98e2bf85c0c3f646df9fe0ac0babf5cacdc97572
dcherian <deepak@cherian.net>
2021-04-16 16:31:39 -0600

    Add var std min max

--- dask_groupby/aggregations.py
+++ dask_groupby/aggregations.py
@@ -1,3 +1,8 @@
+from functools import partial
+
+import numpy as np
+
+
 def _atleast_1d(inp):
     if isinstance(inp, str):
         inp = (inp,)
@@ -16,11 +21,19 @@ class Aggregation:
         # final aggregation
         self.aggregate = aggregate if aggregate else combine
         # finalize results (see mean)
-        self.finalize = finalize if finalize else lambda x: x[self.chunk[0]]
+        self.finalize = finalize if finalize else lambda x: x["intermediates"][0]
         # fill_value is used to reindex to expected_groups.
         # They should make sense when aggregated together with results from other blocks
         self.fill_value = fill_value
         self.dtype = dtype
+
+
+def sum_of_squares(array, axis=-1):
+    return np.sum(array ** 2, axis)
+
+
+def nansum_of_squares(array, axis=-1):
+    return np.nansum(array ** 2, axis)
 
 
 count = Aggregation("count", chunk="count", combine="sum", fill_value=0, dtype=int)
@@ -32,13 +45,73 @@ mean = Aggregation(
     "mean",
     chunk=("sum", "count"),
     combine=("sum", "sum"),
-    finalize=lambda x: x["sum"] / x["count"],
+    finalize=lambda sum_, count: sum_ / count,
     fill_value=0,
 )
 nanmean = Aggregation(
     "nanmean",
     chunk=("nansum", "count"),
     combine=("sum", "sum"),
-    finalize=lambda x: x["nansum"] / x["count"],
+    finalize=lambda sum_, count: sum_ / count,
     fill_value=0,
+)
+
+
+# TODO: fix this for complex numbers
+def _var_finalize(sumsq, sum_, count, ddof=0):
+    result = (sumsq - (sum_ ** 2 / count)) / (count - ddof)
+    result[(count - ddof) <= 0] = np.nan
+    return result
+
+
+def _std_finalize(sumsq, sum_, count, ddof=0):
+    return np.sqrt(_var_finalize(sumsq, sum_, count, ddof))
+
+
+var = Aggregation(
+    "var",
+    chunk=(sum_of_squares, "sum", "count"),
+    combine=("sum", "sum", "sum"),
+    finalize=_var_finalize,
+    fill_value=0,
+)
+nanvar = Aggregation(
+    "nanvar",
+    chunk=(nansum_of_squares, "count"),
+    combine=("sum", "sum"),
+    finalize=_var_finalize,
+    fill_value=0,
+)
+std = Aggregation(
+    "std",
+    chunk=(sum_of_squares, "sum", "count"),
+    combine=("sum", "sum", "sum"),
+    finalize=_std_finalize,
+    fill_value=0,
+)
+nanstd = Aggregation(
+    "nanstd",
+    chunk=(nansum_of_squares, "count"),
+    combine=("sum", "sum"),
+    finalize=_std_finalize,
+    fill_value=0,
+)
+
+
+# TODO: How does this work if data has fillvalue?
+def _minmax_finalize(data, fv):
+    data[data == fv] = np.nan
+    return data
+
+
+_min_finalize = partial(_minmax_finalize, fv=np.inf)
+_max_finalize = partial(_minmax_finalize, fv=-np.inf)
+
+min = Aggregation("min", chunk="min", combine="min", fill_value=np.inf, finalize=_min_finalize)
+nanmin = Aggregation(
+    "nanmin", chunk="nanmin", combine="min", fill_value=np.inf, finalize=_min_finalize
+)
+max = Aggregation("max", chunk="max", combine="max", fill_value=-np.inf, finalize=_max_finalize)
+nanmax = Aggregation(
+    "nanmax", chunk="nanmax", combine="max", fill_value=-np.inf, finalize=_max_finalize
 )
