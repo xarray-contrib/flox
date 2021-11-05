d15baee98f2af69a4983e3a6df89c51c30fcde21
dcherian <deepak@cherian.net>
2021-11-05 11:47:49 -0600

    Updated packaging

--- dask_groupby/__init__.py
+++ dask_groupby/__init__.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # flake8: noqa
 """Top-level module for dask_groupby ."""
-from pkg_resources import DistributionNotFound, get_distribution
-
 from .core import groupby_reduce  # noqa
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    _version__ = "unknown"
+    from importlib.metadata import version as _version
+except ImportError:
+    # if the fallback library is missing, we are doomed.
+    from importlib_metadata import version as _version  # type: ignore[no-redef]
+
+try:
+    __version__ = _version("dask_groupby")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
