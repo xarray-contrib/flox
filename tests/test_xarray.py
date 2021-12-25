2cef60ea7fcea1a875dd664c00a487882aa18c41
Deepak Cherian <dcherian@users.noreply.github.com>
2021-12-24 19:54:58 -0700

    Fix xarray_reduce when `by` has NaN. (#54)
    * Fix xarray_reduce when `by` has NaN.
    
    * fix test

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -56,6 +56,14 @@ def test_xarray_reduce(skipna, add_nan, min_count, engine):
     )
     assert_equal(expected, actual)
 
+    da["labels2"] = da.labels2.astype(float)
+    da["labels2"][0] = np.nan
+    expected = da.groupby("labels2").sum(skipna=skipna, min_count=min_count)
+    actual = xarray_reduce(
+        da, "labels2", func="sum", skipna=skipna, min_count=min_count, engine=engine
+    )
+    assert_equal(expected, actual)
+
     # test dimension ordering
     # actual = xarray_reduce(
     #    da.transpose("y", ...), "labels", func="sum", skipna=skipna, min_count=min_count
@@ -175,7 +183,7 @@ def test_xarray_reduce_errors():
         xarray_reduce(da, by, func="mean")
 
     by.name = "by"
-    with pytest.raises(ValueError, match="cannot reduce over"):
+    with pytest.raises(ValueError, match="Cannot reduce over"):
         xarray_reduce(da, by, func="mean", dim="foo")
 
     if has_dask:
