b0dcf33e0538bb1e730b825ff0d93123cb7a142c
dcherian <deepak@cherian.net>
2022-01-03 11:21:40 -0700

    rename cache name

--- flox/cache.py
+++ flox/cache.py
@@ -5,7 +5,7 @@ try:
     import dask
 
     # 1MB cache
-    cohorts_cache = cachey.Cache(1e6)
-    memoize = partial(cohorts_cache.memoize, key=dask.base.tokenize)
+    cache = cachey.Cache(1e6)
+    memoize = partial(cache.memoize, key=dask.base.tokenize)
 except ImportError:
     memoize = lambda x: x
