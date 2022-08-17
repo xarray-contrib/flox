e40408e7a763b10266fbca33d9e63ce5966a07ac
Illviljan <14371165+Illviljan@users.noreply.github.com>
2022-08-17 21:01:57 +0200

    Restart with current benchmarks

--- asv_bench/benchmarks/reduce.py
+++ asv_bench/benchmarks/reduce.py
@@ -1,33 +1,44 @@
 import numpy as np
+import numpy_groupies as npg
 
 import flox
 
 from . import parameterized
 
 N = 1000
+funcs = ["sum", "nansum", "mean", "nanmean", "argmax", "max"]
+engines = ["flox", "numpy"]
 
 
 class ChunkReduce:
     """Time the core reduction function."""
 
     def setup(self, *args, **kwargs):
+        # pre-compile jitted funcs
+        if "numba" in engines:
+            for func in funcs:
+                npg.aggregate_numba.aggregate(
+                    np.ones((100,), dtype=int), np.ones((100,), dtype=int), func=func
+                )
         raise NotImplementedError
 
-    @parameterized("func", ["sum", "nansum", "mean", "nanmean", "argmax"])
-    def time_reduce(self, func):
+    @parameterized("func, engine", [funcs, engines])
+    def time_reduce(self, func, engine):
         flox.groupby_reduce(
             self.array,
             self.labels,
             func=func,
+            engine=engine,
             axis=self.axis,
         )
 
-    @parameterized("func", ["sum", "nansum", "mean", "nanmean", "argmax"])
-    def peakmem_reduce(self, func):
+    @parameterized("func, engine", [funcs, engines])
+    def peakmem_reduce(self, func, engine):
         flox.groupby_reduce(
             self.array,
             self.labels,
             func=func,
+            engine=engine,
             axis=self.axis,
         )
 
