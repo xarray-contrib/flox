5179fc50e41afb674793d54fd275cc9b6adeb909
Illviljan <14371165+Illviljan@users.noreply.github.com>
2022-08-17 21:21:58 +0200

    Don't test argmax until it's supported in "flox" engine

--- asv_bench/benchmarks/reduce.py
+++ asv_bench/benchmarks/reduce.py
@@ -6,7 +6,7 @@ import flox
 from . import parameterized
 
 N = 1000
-funcs = ["sum", "nansum", "mean", "nanmean", "argmax", "max"]
+funcs = ["sum", "nansum", "mean", "nanmean", "max"]
 engines = ["flox", "numpy"]
 
 
