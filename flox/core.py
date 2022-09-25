70bcfd4b803594872db545db791356d26f48376e
Illviljan <14371165+Illviljan@users.noreply.github.com>
2022-09-25 16:01:17 +0200

    Missed a if !

--- flox/core.py
+++ flox/core.py
@@ -1151,13 +1151,14 @@ def dask_groupby_agg(
         )
     else:
         intermediate = applied
-        # if is_duck_dask_array(by_input):
-        #     expected_groups = None
-        #     # group_chunks = ((np.nan,),)
-        # else:
-        #     expected_groups = _get_expected_groups(by_input, sort=sort)
-        #     # group_chunks = ((len(expected_groups),),)
-        expected_groups = _get_expected_groups(by_input, sort=sort, raise_if_dask=False)
+        if expected_groups is None:
+            if is_duck_dask_array(by_input):
+                expected_groups = None
+                # group_chunks = ((np.nan,),)
+            else:
+                expected_groups = _get_expected_groups(by_input, sort=sort)
+                # group_chunks = ((len(expected_groups),),)
+            # expected_groups = _get_expected_groups(by_input, sort=sort, raise_if_dask=False)
         group_chunks = ((len(expected_groups),) if expected_groups is not None else (np.nan,),)
 
     if method == "map-reduce":
