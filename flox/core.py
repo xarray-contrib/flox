cd9b838e904f1f708f572163a5436e378ee6fd62
dcherian <deepak@cherian.net>
2022-10-11 19:19:26 -0600

    Fix typing

--- flox/core.py
+++ flox/core.py
@@ -1229,7 +1229,9 @@ def dask_groupby_agg(
             expected_groups = None
         else:
             expected_groups = _get_expected_groups(by_input, sort=sort)
-    group_chunks = ((len(expected_groups),) if expected_groups is not None else (np.nan,),)
+    group_chunks: tuple[tuple[Union[int, float], ...]] = (
+        (len(expected_groups),) if expected_groups is not None else (np.nan,),
+    )
 
     if method in ["map-reduce", "cohorts", "split-reduce"]:
         combine: Callable[..., IntermediateDict]
