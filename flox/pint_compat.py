70aa03a8065d0113ddef69eb87935642b3914385
dcherian <deepak@cherian.net>
2023-01-24 20:26:39 -0700

    Fix test

--- flox/pint_compat.py
+++ flox/pint_compat.py
@@ -7,7 +7,7 @@ def _strip_units(*arrays):
     except ImportError:
         pint_quantity = None
 
-    bare = [array.magnitude if isinstance(array, pint_quantity) else array for array in arrays]
+    bare = tuple(array.magnitude if isinstance(array, pint_quantity) else array for array in arrays)
     units = [array.units if isinstance(array, pint_quantity) else None for array in arrays]
 
     return *bare, units
@@ -17,9 +17,9 @@ def _reattach_units(*arrays, units):
     try:
         import pint
 
-        return [
+        return tuple(
             pint.Quantity(array, unit) if unit is not None else array
             for array, unit in zip(arrays, units)
-        ]
+        )
     except ImportError:
         return arrays
