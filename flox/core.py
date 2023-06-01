7808b7546a456129141032fc444d85a4b3b4afc1
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-06-02 00:51:54 +0200

    Update core.py

--- flox/core.py
+++ flox/core.py
@@ -1616,22 +1616,19 @@ def _convert_expected_groups_to_index(
 ) -> T_ExpectIndexOptTuple:
     out: list[T_ExpectIndexOpt] = []
     for ex, isbin_ in zip(expected_groups, isbin):
-        reveal_type(ex)
         if isinstance(ex, pd.IntervalIndex) or (isinstance(ex, pd.Index) and not isbin_):
-            # if isinstance(ex, pd.Index) and (isinstance(ex, pd.IntervalIndex) or not isbin_):
-            reveal_type(ex)
             e = ex
-            reveal_type(e)
             if sort:
                 e = e.sort_values()
             out.append(e)
         elif ex is not None:
+            e = ex
             if isbin_:
-                out.append(pd.IntervalIndex.from_breaks(ex))
+                out.append(pd.IntervalIndex.from_breaks(e))
             else:
                 if sort:
-                    ex = np.sort(ex)
-                out.append(pd.Index(ex))
+                    e = np.sort(e)
+                out.append(pd.Index(e))
         else:
             assert ex is None
             out.append(None)
