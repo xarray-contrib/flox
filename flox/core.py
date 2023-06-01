b346137069d55103f239cbcc7b4a654774a56d20
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-06-01 23:04:18 +0200

    Update core.py

--- flox/core.py
+++ flox/core.py
@@ -224,8 +224,13 @@ def find_group_cohorts(labels, chunks, merge: bool = True) -> dict:
     # these are chunks where a label is present
     label_chunks = pd.Series(which_chunk).groupby(raveled).unique()
     # These invert the label_chunks mapping so we know which labels occur together.
+    def test(x) -> tuple:
+        a = label_chunks.get(x)
+        reveal_type(a)
+        return tuple(a)
+
     # chunks_cohorts = tlz.groupby(lambda x: (label_chunks.get(x),), label_chunks.keys())
-    chunks_cohorts = tlz.groupby(lambda x: tuple(label_chunks.get(x)), label_chunks.keys())
+    chunks_cohorts = tlz.groupby(test), label_chunks.keys())
 
     if merge:
         # First sort by number of chunks occupied by cohort
