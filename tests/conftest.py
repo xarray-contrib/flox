1410b77c5d8369358d6665ce607784bd26f7e9c3
Deepak Cherian <deepak@cherian.net>
2023-10-02 17:58:31 -0600

    reintroduce engines

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,15 @@
 import pytest
 
-from . import requires_numbagg
+from . import requires_numba, requires_numbagg
 
 
 @pytest.fixture(
     scope="module",
     params=[
-        # "flox",
-        # "numpy",
-        # pytest.param("numba", marks=requires_numba),
-        pytest.param("numbagg", marks=requires_numbagg)
+        "flox",
+        "numpy",
+        pytest.param("numba", marks=requires_numba),
+        pytest.param("numbagg", marks=requires_numbagg),
     ],
 )
 def engine(request):
