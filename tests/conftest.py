d46c3aef3b9c5d9dadd6555c0e7b1b628c0633e6
Deepak Cherian <deepak@cherian.net>
2023-10-05 08:28:05 -0600

    limit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
 import pytest
 
-from . import requires_numba
-
 
 @pytest.fixture(
-    scope="module", params=["flox", "numpy", pytest.param("numba", marks=requires_numba)]
+    scope="module",
+    params=[
+        # "flox",
+        "numpy",
+        # pytest.param("numba", marks=requires_numba)
+    ],
 )
 def engine(request):
     return request.param
