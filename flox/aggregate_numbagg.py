Fix fill_value masking

flox/aggregate_numbagg.py
@@ -4,6 +4,8 @@ import numbagg
 import numbagg.grouped
 import numpy as np
 
+from .core import _unique
+
 DEFAULT_FILL_VALUE = {
     "nansum": 0,
     "nanmean": np.nan,
@@ -37,8 +39,8 @@ def _numbagg_wrapper(
     group_idx,
     array,
     *,
+    func,
     axis=-1,
-    func=None,
     size=None,
     fill_value=None,
     dtype=None,
@@ -63,14 +65,17 @@ def _numbagg_wrapper(
         # The following are unsupported
         # fill_value=fill_value,
         # dtype=dtype,
-    )
+    ).astype(dtype, copy=False)
 
     # The condition needs to be
     # is len(found_groups) < size; if so we mask with fill_value (?)
-    needs_masking = fill_value is not None and fill_value != default_fv
+    needs_masking = fill_value is not None and not np.array_equal(
+        fill_value, default_fv, equal_nan=True
+    )
     if needs_masking:
-        count = numbagg.grouped.group_nancount(array, group_idx, axis=axis, num_labels=size)
-        result[count == 0] = fill_value
+        uniques = _unique(group_idx)
+        mask = np.isin(uniques, np.arange(size), assume_unique=True, invert=True)
+        result[..., uniques[mask]] = fill_value
     return result
 
 