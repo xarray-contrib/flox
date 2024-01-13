22d6838f6ba3c6f676dff5268163bc9c7d6f1dd8
Deepak Cherian <deepak@cherian.net>
2024-01-13 15:09:05 -0700

    Back to full tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,10 @@ from . import requires_numba, requires_numbagg
 @pytest.fixture(
     scope="module",
     params=[
-        # "flox",
+        "flox",
         "numpy",
-        # pytest.param("numba", marks=requires_numba),
-        # pytest.param("numbagg", marks=requires_numbagg),
+        pytest.param("numba", marks=requires_numba),
+        pytest.param("numbagg", marks=requires_numbagg),
     ],
 )
 def engine(request):
