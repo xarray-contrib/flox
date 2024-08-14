edc13445a098ee95f074a4bd92bde1c48694804c
Deepak Cherian <dcherian@users.noreply.github.com>
2024-08-14 13:44:10 -0600

    Expand groupby_reduce property tests (#385)
    * Expand groupby_reduce property tests
    
    * Add back var, std
    
    * cast quantile result
    
    * Revert "Add back var, std"
    
    This reverts commit 805b8d313750232756cc9bf970e8cd472e01995a.
    
    * pin numpy in benchmark env
    
    * Add benchmarks as test

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -61,7 +61,11 @@ def not_overflowing_array(array: np.ndarray[Any, Any]) -> bool:
     return result
 
 
-@given(data=st.data(), array=numeric_arrays, func=func_st)
+@given(
+    data=st.data(),
+    array=st.one_of(numeric_arrays, chunked_arrays(arrays=numeric_arrays)),
+    func=func_st,
+)
 def test_groupby_reduce(data, array, func: str) -> None:
     # overflow behaviour differs between bincount and sum (for example)
     assume(not_overflowing_array(array))
@@ -93,7 +97,13 @@ def test_groupby_reduce(data, array, func: str) -> None:
 
         # numpy-groupies always does the calculation in float64
         if (
-            ("var" in func or "std" in func or "sum" in func or "mean" in func)
+            (
+                "var" in func
+                or "std" in func
+                or "sum" in func
+                or "mean" in func
+                or "quantile" in func
+            )
             and array.dtype.kind == "f"
             and array.dtype.itemsize != 8
         ):
